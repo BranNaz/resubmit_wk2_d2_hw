91d5661c2e54a931cf25a454dad935eabc38f4c9
Brandon Nazario <brandon.nazario21@gmail.com>
2022-07-14 02:27:39 -0400

    re-submition of wk2 d2 hw

---
+++ In_class_work/homework_wk2_d2/brandon_module.py
@@ -0,0 +1,13 @@
+import brandon_module
+import math
+from math import pi
+from math import radians
+
+# diam = 23
+diam = 23
+
+def myMath ():
+    circumfrence = math.pi * diam
+    return circumfrence
+
+
